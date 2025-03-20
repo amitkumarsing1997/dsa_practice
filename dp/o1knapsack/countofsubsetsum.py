b282e2c1fba803c40c45767bc2e2d385696487d0
amit <ak910221@gmail.com>
2025-03-20 13:01:26 -0700

    commit for countofsubsetsum

---
+++ dp/o1knapsack/countofsubsetsum.py
@@ -0,0 +1,29 @@
+"""
+arr = [2,3,5,6,8,10]
+sum = 10
+ans = {2,8} , {5,2,3} , {10}
+final ans = 3
+
+"""
+
+def countofsubsetsum(arr, sum):
+    n = len(arr)
+    t = [ [-1] * (sum + 1) for i in range(0, n+1)]
+    for i in range(0, n + 1):
+        for j in range(0, sum + 1):
+            if i == 0:
+                t[i][j] = 0
+            if j == 0:
+                t[i][j] = 1
+    for i in range(1, n + 1):
+        for j in range(1, sum + 1):
+            if arr[i-1] <= j:
+                t[i][j] = t[i-1][j-arr[i-1]] + t[i-1][j]
+            else:
+                t[i][j] = t[i-1][j]
+    return t[n][sum]
+
+
+arr = [2, 3, 5, 6, 8, 10]
+sum = 10
+print(countofsubsetsum(arr, sum))
